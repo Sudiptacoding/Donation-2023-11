6fecb02614ef832b46af02c460c36620b08158c4
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-09-26 10:23:46 +0600

    Localstore function complet

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,8 +22,6 @@ const Home = () => {
     }, []);
 
     const handelClick = () => {
-
-
         if (input) {
             const filterData = duplicate.filter(item => item.category.toLowerCase() === input.toLowerCase())
             console.log(filterData)
